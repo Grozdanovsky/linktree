1e665d03e60ffcebb8c4d4127733724993a56732
Grozdanovsky <viktor.grozdanovski@outlook.com>
2022-05-24 12:23:13 +0200

    some commit

--- main.py
+++ main.py
@@ -1,62 +1,109 @@
-
 import requests
 from bs4 import BeautifulSoup
 import json
 import pprint
 from lxml import etree
 from io import StringIO
+import time
+import csv
+import json
+start = time.time()
+
+# with open('users.csv', 'r') as f:
+#     usernames = f.readlines()
+#     first_100 = usernames[:100]
+#     for username in first_100:
+#         username = username.split(',')
+#         username = username[1]
 
 
-links = ['katyperry']
-full_names = []
-full_links = []
-tiers = []
-for ending_link in links:
 
 
-    url = f"https://linktr.ee/{ending_link}"
+
+f = open('linktree_users.csv', 'a',newline="",encoding='utf-8')
+# writer = csv.DictWriter(f,fieldnames = ['user','description','other_links','social_links','tier','sensitive_content'])
+# writer.writeheader()
+# for username in usernames:
+
+
+# users = ["katyperry"]
+users = open('results.csv','r')
+for username in users:
+
+    url = f"linktr.ee/{username}"
     parser = etree.HTMLParser()
     r = requests.get(url)
     html = r.content.decode("utf-8")
     tree = etree.parse(StringIO(html), parser=parser)
     soup = BeautifulSoup(html,"html.parser")
-
-
-    #get everything from the script
-    everything = soup.find('script', {"id" : "__NEXT_DATA__"})
-
-    #make the data to json
-    data = json.loads(everything.text)
-
-
-    #get the username 
-    name = data.get('props',{}).get('pageProps',{}).get('account',{}).get('pageTitle')
-    full_names.append(name)
-
-    #get all the social links
-    social_links = data.get('props',{}).get('pageProps',{}).get('account',{}).get("socialLinks")
     
-    for item in range(len(social_links)):
+    
+    if r.status_code ==404 or r.status_code == 429:
+        continue
+
+    else:
+        try:
+            other_links = []
+            social_media_links = []
+            
+                    
+              
+            everything =soup.find('script', {"id" : "__NEXT_DATA__"})
+            data = json.loads(everything.text)
+           
+            
+            
+
+           
+            user = (data.get('props',{}).get('pageProps',{}).get('account',{}).get('username'))
+
+                             
+            description = data.get('props',{}).get('pageProps',{}).get('description')
+            
+
         
-        linkce =  social_links[item-1].get('url')
-        full_links.append(linkce)
+            links = data.get('props',{}).get('pageProps',{}).get('account',{}).get("links")
+            
+            
+            for username in range(len(links)):
+                dict1= {}
+                key = links[username-1].get('title')
+                value = links[username-1].get('url')
+                dict1[key] = value
+                other_links.append(dict1)
+                
+         
+            social_links = data.get('props',{}).get('pageProps',{}).get('account',{}).get("socialLinks")   
+                         
+            for username in range(len(social_links)):                  
+                social_media_links.append(social_links[username-1].get('url'))
+                
+            
+                
+                                        
+                                        
 
-    #get the tier for the user
-    links = data.get('props',{}).get('pageProps',{}).get('account',{}).get("links")
+            tier = data.get('props',{}).get('pageProps',{}).get('account',{}).get('tier')
+                                    # tiers.append(tier)
 
+                    
+            # email_verified= data.get('props',{}).get('pageProps',{}).get('account',{}).get('owner',{}).get('isEmailVerified')
+            # profile_verified = data.get('props',{}).get('pageProps',{}).get('isProfileVerified')
 
-    for item in range(len(links)):
-        linkce = links[item-1].get('url')
-        full_links.append(linkce)
+            sensitive_content = data.get('props',{}).get('pageProps',{}).get('hasSensitiveContent')
 
+            tup1 = (user,description,other_links,social_media_links,tier,sensitive_content)
+            writer = csv.writer(f)
+            writer.writerow(tup1)
+        except TypeError:
+            continue
+        except AttributeError:
+            continue
+        
+end = time.time()
 
-    tier = data.get('props',{}).get('pageProps',{}).get('account',{}).get('tier')
-    tiers.append(tier)
-
-print(full_names)
-print(full_links)
-print(tiers)
-
+f.close()
+print(f'time it took  {end - start}' )
 
 
  
