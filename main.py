96adba4bcfd6b0dc65b736effad87ec05f8c2258
Grozdanovsky <viktor.grozdanovski@outlook.com>
2022-05-26 13:18:00 +0200

    some improvement

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import time
 import requests
 import concurrent.futures
+from concurrent.futures import  ThreadPoolExecutor , as_completed
 from lxml import etree
 from io import StringIO
 from bs4 import BeautifulSoup
@@ -11,15 +12,17 @@ from bs4 import BeautifulSoup
 start = time.time()
 MAX_THREADS = 4
 
-with  open('testing_users.csv', 'a', newline="", encoding='utf-8') as f: #puy the name of the file that you want to append to
-    writer = csv.DictWriter(f,fieldnames = ['user','description','other_links','social_links','tier','sensitive_content'])
-    writer.writeheader()
+f = open('testing_users.csv', 'a', newline="", encoding='utf-8') #put the name of the file that you want to append to
+writer = csv.DictWriter(f,fieldnames = ['user','description','other_links','social_links','tier','sensitive_content'])
+writer.writeheader()
 
 def download_users(flat_list):
     threads = min(MAX_THREADS, len(flat_list))
     
     with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
         executor.map(transform,flat_list)
+    
+    
 
 def transform(username):
 
@@ -29,18 +32,19 @@ def transform(username):
     html = r.content.decode("utf-8")
     tree = etree.parse(StringIO(html), parser=parser)
     soup = BeautifulSoup(html, "html.parser")
-    print(username)
+    
     everything = soup.find('script', {"id": "__NEXT_DATA__"})
     data = json.loads(everything.text)
+    
     
     if r.status_code == 404:
         print('Error user not found!',username)
      
-    elif r.status_code == 429:
+    if r.status_code == 429:
         print("Too many requests",username)  
          
 
-    elif r.status_code == 200:
+    if r.status_code == 200:
         try:
             print("User found!",username)
             other_links = []
@@ -95,5 +99,7 @@ if __name__ == '__main__':
         print(len(flat_list))
         download_users(flat_list)
 
+
+f.close()
 end = time.time()
 print(f'time it took  {end - start}')
