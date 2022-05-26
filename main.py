371ede03fd60241bb40e1a98f6987f279e2c3f61
Grozdanovsky <viktor.grozdanovski@outlook.com>
2022-05-26 11:52:23 +0200

    Updateded code with functions and some multithreading

--- main.py
+++ main.py
@@ -1,109 +1,99 @@
-import requests
-from bs4 import BeautifulSoup
-import json
-import pprint
-from lxml import etree
-from io import StringIO
-import time
 import csv
 import json
+import time
+import requests
+import concurrent.futures
+from lxml import etree
+from io import StringIO
+from bs4 import BeautifulSoup
+
+
 start = time.time()
+MAX_THREADS = 4
 
-# with open('users.csv', 'r') as f:
-#     usernames = f.readlines()
-#     first_100 = usernames[:100]
-#     for username in first_100:
-#         username = username.split(',')
-#         username = username[1]
+with  open('testing_users.csv', 'a', newline="", encoding='utf-8') as f: #puy the name of the file that you want to append to
+    writer = csv.DictWriter(f,fieldnames = ['user','description','other_links','social_links','tier','sensitive_content'])
+    writer.writeheader()
 
+def download_users(flat_list):
+    threads = min(MAX_THREADS, len(flat_list))
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+        executor.map(transform,flat_list)
 
+def transform(username):
 
-
-
-f = open('linktree_users.csv', 'a',newline="",encoding='utf-8')
-# writer = csv.DictWriter(f,fieldnames = ['user','description','other_links','social_links','tier','sensitive_content'])
-# writer.writeheader()
-# for username in usernames:
-
-
-# users = ["katyperry"]
-users = open('results.csv','r')
-for username in users:
-
-    url = f"linktr.ee/{username}"
+    url = f"https://linktr.ee/{username}"
     parser = etree.HTMLParser()
     r = requests.get(url)
     html = r.content.decode("utf-8")
     tree = etree.parse(StringIO(html), parser=parser)
-    soup = BeautifulSoup(html,"html.parser")
+    soup = BeautifulSoup(html, "html.parser")
+    print(username)
+    everything = soup.find('script', {"id": "__NEXT_DATA__"})
+    data = json.loads(everything.text)
     
-    
-    if r.status_code ==404 or r.status_code == 429:
-        continue
+    if r.status_code == 404:
+        print('Error user not found!',username)
+     
+    elif r.status_code == 429:
+        print("Too many requests",username)  
+         
 
-    else:
+    elif r.status_code == 200:
         try:
+            print("User found!",username)
             other_links = []
             social_media_links = []
-            
-                    
-              
-            everything =soup.find('script', {"id" : "__NEXT_DATA__"})
-            data = json.loads(everything.text)
-           
-            
-            
-
-           
-            user = (data.get('props',{}).get('pageProps',{}).get('account',{}).get('username'))
-
-                             
-            description = data.get('props',{}).get('pageProps',{}).get('description')
-            
-
-        
-            links = data.get('props',{}).get('pageProps',{}).get('account',{}).get("links")
-            
-            
+            user = (data.get('props', {}).get('pageProps', {}).get(
+            'account', {}).get('username'))
+    
+            description = data.get('props', {}).get('pageProps', {}).get('description')
+    
+            links = data.get('props', {}).get(
+            'pageProps', {}).get('account', {}).get("links")
+    
             for username in range(len(links)):
-                dict1= {}
+                dict1 = {}
                 key = links[username-1].get('title')
                 value = links[username-1].get('url')
                 dict1[key] = value
                 other_links.append(dict1)
-                
-         
-            social_links = data.get('props',{}).get('pageProps',{}).get('account',{}).get("socialLinks")   
-                         
-            for username in range(len(social_links)):                  
-                social_media_links.append(social_links[username-1].get('url'))
-                
             
-                
-                                        
-                                        
-
-            tier = data.get('props',{}).get('pageProps',{}).get('account',{}).get('tier')
-                                    # tiers.append(tier)
-
-                    
-            # email_verified= data.get('props',{}).get('pageProps',{}).get('account',{}).get('owner',{}).get('isEmailVerified')
-            # profile_verified = data.get('props',{}).get('pageProps',{}).get('isProfileVerified')
-
-            sensitive_content = data.get('props',{}).get('pageProps',{}).get('hasSensitiveContent')
-
-            tup1 = (user,description,other_links,social_media_links,tier,sensitive_content)
+            social_links = data.get('props', {}).get(
+                'pageProps', {}).get('account', {}).get("socialLinks")
+            
+            for username in range(len(social_links)):
+                social_media_links.append(social_links[username-1].get('url'))
+            
+            tier = data.get('props', {}).get(
+                'pageProps', {}).get('account', {}).get('tier')
+            
+            sensitive_content = data.get('props', {}).get(
+                'pageProps', {}).get('hasSensitiveContent')
+            
+            tup1 = (user, description, other_links,
+                    social_media_links, tier, sensitive_content)
+            
             writer = csv.writer(f)
             writer.writerow(tup1)
+            
+            
         except TypeError:
-            continue
+            print('TypeError')
+            
         except AttributeError:
-            continue
-        
+            print('AttributeError') 
+
+
+if __name__ == '__main__':
+
+    with open('new_users.csv','r') as users: #this is the file that you read from
+        csv_reader = csv.reader(users)
+        list_of_users = list(csv_reader)
+        flat_list = [item for sublist in list_of_users for item in sublist]
+        print(len(flat_list))
+        download_users(flat_list)
+
 end = time.time()
-
-f.close()
-print(f'time it took  {end - start}' )
-
-
- 
+print(f'time it took  {end - start}')
